c0154c4161b96528dd9303c7b85ca03712ecbdcb
Urias01 <uriasluis64@gmail.com>
2026-05-26 21:11:42 -0300

    feat: add sign in to controller

--- src/main/java/com/s/commerce/presentation/auth/controllers/AuthController.java
+++ src/main/java/com/s/commerce/presentation/auth/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.s.commerce.presentation.auth.controllers;
 
+import com.s.commerce.application.authentication.signIn.SignInRequest;
+import com.s.commerce.application.authentication.signIn.SignInResponse;
+import com.s.commerce.application.authentication.signIn.SignInUseCase;
 import com.s.commerce.application.commons.response.ApiResponse;
 import com.s.commerce.application.user.create.CreateUserRequest;
 import com.s.commerce.application.user.create.CreateUserResponse;
@@ -13,15 +16,23 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final CreateUserUseCase createUserUseCase;
+    private final SignInUseCase signInUseCase;
 
-    public AuthController(CreateUserUseCase createUserUseCase) {
+    public AuthController(CreateUserUseCase createUserUseCase, SignInUseCase signInUseCase) {
         this.createUserUseCase = createUserUseCase;
+        this.signInUseCase = signInUseCase;
     }
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<CreateUserResponse>> createUser(@RequestBody CreateUserRequest request) {
         CreateUserResponse response = this.createUserUseCase.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+    }
+
+    @PostMapping("/signing")
+    public ResponseEntity<ApiResponse<SignInResponse>> signIn(@RequestBody SignInRequest request) {
+        SignInResponse response = this.signInUseCase.execute(request);
+        return ResponseEntity.ok().body(ApiResponse.success(response));
     }
 
 }
