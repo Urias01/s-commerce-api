7cfc7241dccec1f17870d3a6759250b6134eaed1
Urias01 <uriasluis64@gmail.com>
2026-06-26 16:13:04 -0300

    chore: add response status to exceptions

--- src/main/java/com/s/commerce/domain/common/exceptions/InvalidOperationException.java
+++ src/main/java/com/s/commerce/domain/common/exceptions/InvalidOperationException.java
@@ -1,5 +1,9 @@
 package com.s.commerce.domain.common.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
 public class InvalidOperationException extends DomainException {
     public InvalidOperationException(String message) {
         super(message);
--- src/main/java/com/s/commerce/exceptions/GlobalExceptions.java
+++ src/main/java/com/s/commerce/exceptions/GlobalExceptions.java
@@ -7,9 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptions {
+@RestController
+public class GlobalExceptions extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ApiResponse<?>> handleDomain(DomainException e) {
